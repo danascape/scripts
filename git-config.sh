5283cc123655c2fd3e674869e36daed8525e39d1
iamsaalim <40296389+iamsaalim@users.noreply.github.com>
2019-10-02 00:38:02 +0530

    add iamsaalim

--- git-config.sh
+++ git-config.sh
@@ -1,3 +1,3 @@
-git config --global user.email "raza231198@gmail.com"
-git config --global user.name "MD Raza"
+git config --global user.email "saalimquadri1@gmail.com"
+git config --global user.name "iamsaalim"
 git config --global core.editor "nano"
