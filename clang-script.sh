c7b13143328da197ad1893cced19b8b18f9855d9
danielle <saalimquadri1@gmail.com>
2019-11-30 11:56:36 +0530

    move to manual exports

--- clang-script.sh
+++ clang-script.sh
@@ -6,6 +6,10 @@ mkdir out
 export ARCH=arm64
 export SUBARCH=arm64
 export AR=llvm-ar
+export NM=llvm-nm
+export OBJCOPY=llvm-objcopy
+export OBJDUMP=llvm-objdump
+export STRIP=llvm-strip
 #export DTC_EXT=dtc
 
 #Make config
@@ -18,8 +22,4 @@ make -j20 O=out \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE="$HOME/tc/aarch64-linux-android-4.9/bin/aarch64-linux-android-" \
                       CROSS_COMPILE_ARM32="$HOME/tc/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
-                      NM="llvm-nm"
-                      OBJCOPY="llvm-objcopy"
-                      OBJDUMP="llvm-objdump"
-                      STRIP="llvm-strip"
 
