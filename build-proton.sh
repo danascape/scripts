edabaeaf44047bf87eefb60754b3435a65d0368d
danielle <saalimquadri1@gmail.com>
2020-04-12 19:03:11 +0530

    update script for Travis

--- build-proton.sh
+++ build-proton.sh
@@ -5,16 +5,19 @@ KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
 CONFIG_PATH=$KERNEL_DIR/arch/arm64/configs/$CONFIG
 
 # download proton
-wget -O proton.tar.zst https://kdrag0n.dev/files/redirector/proton_clang-latest.tar.zst
-mkdir -p ../toolchain/clang
-tar -I zstd -xvf *.tar.zst -C ../toolchain/clang --strip-components=1
+#wget -O proton.tar.zst https://kdrag0n.dev/files/redirector/proton_clang-latest.tar.zst
+#mkdir -p ../toolchain/clang
+#tar -I zstd -xvf *.tar.zst -C ../toolchain/clang --strip-components=1
 
 # paths setup
-export PATH="$HOME/kernel/toolchain/clang/bin:$PATH"
-export LD_LIBRARY_PATH="$HOME/kernel/toolchain/clang/lib:$HOME/kernel/toolchain/clang/lib64:$LD_LIBRARY_PATH"
-export KBUILD_COMPILER_STRING="$($HOME/kernel/toolchain/clang/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
+export PATH="$HOME/clang/bin:$PATH"
+export LD_LIBRARY_PATH="$HOME/clang/lib:$HOME/clang/lib64:$LD_LIBRARY_PATH"
+export KBUILD_COMPILER_STRING="$($HOME/clang/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
 export KBUILD_BUILD_USER="saalim"
-export KBUILD_BUILD_HOST="netcup"
+export KBUILD_BUILD_HOST="travis"
+
+make O=out ARCH=arm64 rosy-perf_defconfig
+
 
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
