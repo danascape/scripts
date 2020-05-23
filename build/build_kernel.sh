440e14bc3b2ee4a1e6f63be45dfb57bd29bd7b34
iamsaalim <saalimquadri1@gmail.com>
2020-05-23 11:46:21 +0200

    build: add an empty kernel build script
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

---
+++ build/build_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2020 Saalim Quadri <saalimquadri1@gmail.com>
+# Copyright (C) 2019 Shivam Kumar Jha <jha.shivam3@gmail.com>
+#
+# Build functions
+
+echo -e "currently WIP"
