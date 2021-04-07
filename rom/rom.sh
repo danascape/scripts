f1bb3da3d718a22e669615921afa17b685d9b001
danascape <saalim.priv@gmail.com>
2021-04-07 12:49:47 +0530

    rom: Take in package name as an argument

--- rom/rom.sh
+++ rom/rom.sh
@@ -2,23 +2,23 @@
 
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# Copyright (C) 2020 Saalim Quadri <saalimquadri1@gmail.com>
+# Copyright (C) 2020-21 Saalim Quadri <saalim.priv@gmail.com>
 #
 
 # Arguements check
-if [ -z ${1} ] || [ -z ${2} ] || [ -z ${3} ]; then
-	echo -e "Usage: bash rom.sh <rom-name> <device-name> <variant> "
+if [ -z ${1} ] || [ -z ${2} ] || [ -z ${3} ] || [ -z ${4} ]; then
+	echo -e "Usage: bash rom.sh <rom-name> <device-name> <variant> <package-name>"
 	exit 1
 fi
 
 # Store some VARS
-PROJECT_DIR="$HOME/weeb/script"
-ROM="$1"
-DEVICE="$2"
-VARIANT="$3"
-ROM_DIR=
-CHAT_ID=
 BOT_API_TOKEN=
+CHAT_ID=
+DEVICE="$2"
+PACKAGE_NAME="$4"
+ROM="$1"
+ROM_DIR="$PWD" # By default
+VARIANT="$3"
 
 # Go to source
 cd "$ROM_DIR"
@@ -36,11 +36,12 @@ fi
 lunch $ROM_$DEVICE-$VARIANT | grep TARGET_PRODUCT
 
 # Start the build
-make otapackage -j$(nproc --all) >logs.txt
+make $PACKAGE_NAME -j$(nproc --all) >logs.txt
 
 # Check if build is done
 if [ -f $ROM_DIR/out/target/product/$DEVICE/*.zip ]; then
 	ZIP=$(echo $ROM_DIR/out/target/product/$DEVICE/*$ROM*.zip)
+	echo "\n Build Complete"
 	echo $ZIP
 	exit 1
 else
