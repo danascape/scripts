ef9f7bf84b6785d7520ff51becaed3a9227fdaf5
Saalim Quadri <danascape@gmail.com>
2023-07-14 08:42:15 +0530

    scripts: Add script to setup minimal build env
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

---
+++ setup/build_env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2019 Salim Quadri <danascape@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+sudo apt update
+
+sudo DEBIAN_FRONTEND=noninteractive \
+	apt install -y \
+	build-essential bc python curl \
+	git zip ftp gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi \
+	libssl-dev lftp zstd wget libelf-dev libfl-dev clang flex bison cpio
