b26f8fac1e6040fcda5375fdd0da86283c9e8d0c
Saalim Quadri <danascape@gmail.com>
2023-09-14 19:44:35 +0530

    qemu: Run arguments correctly
    * Also drop from shellcheck for now
    
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- qemu/start-qemu.sh
+++ qemu/start-qemu.sh
@@ -6,13 +6,14 @@
 argument="$1"
 drive_args="file=/home/saalim/Desktop/buildroot/buildroot/output/images/rootfs.ext2,if=virtio,format=raw"
 kernel="arch/x86_64/boot/bzImage"
+EXTRA_ARGS=""
 
 case "$argument" in
 	--gdb)
 		{
-			EXTRA_ARGS='-s -S'
+			EXTRA_ARGS="-s -S"
 		}
 	;;
 esac
 
-qemu-system-x86_64 -M pc -kernel $kernel -drive $drive_args -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user -serial stdio -display none -m 2048M,slots=2,maxmem=2176M "${EXTRA_ARGS}"
+qemu-system-x86_64 -M pc -kernel $kernel -drive $drive_args -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user -serial stdio -display none -m 2048M,slots=2,maxmem=2176M ${EXTRA_ARGS}
