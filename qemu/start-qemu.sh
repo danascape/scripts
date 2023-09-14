04afb59dd31bff8cf831d7df585a98af33722d65
Saalim Quadri <danascape@gmail.com>
2023-09-14 19:34:30 +0530

    qemu: Add gdb argument
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- qemu/start-qemu.sh
+++ qemu/start-qemu.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2019 Salim Quadri <danascape@gmail.com>
+# Copyright (C) 2019 Saalim Quadri <danascape@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-qemu-system-x86_64 \
-	-M pc \
-	-kernel arch/x86_64/boot/bzImage \
-	-drive file=/home/saalim/Desktop/buildroot/buildroot/output/images/rootfs.ext2,if=virtio,format=raw \
-	-append "rootwait root=/dev/vda console=tty1 console=ttyS0" \
-	-net nic,model=virtio -net user \
-	-serial stdio \
-	-display none \
-	-m 2048M,slots=2,maxmem=2176M
+argument="$1"
+drive_args="file=/home/saalim/Desktop/buildroot/buildroot/output/images/rootfs.ext2,if=virtio,format=raw"
+kernel="arch/x86_64/boot/bzImage"
 
+case "$argument" in
+	--gdb)
+		{
+			EXTRA_ARGS='-s -S'
+		}
+	;;
+esac
+
+qemu-system-x86_64 -M pc -kernel $kernel -drive $drive_args -append "rootwait root=/dev/vda console=tty1 console=ttyS0" -net nic,model=virtio -net user -serial stdio -display none -m 2048M,slots=2,maxmem=2176M ${EXTRA_ARGS}
--- setup/build_env.sh
+++ setup/build_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2019 Salim Quadri <danascape@gmail.com>
+# Copyright (C) 2019 Saalim Quadri <danascape@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 sudo apt update
