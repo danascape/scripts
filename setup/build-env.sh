4ca4d327fb858e3b2cb7ec788b2bd6acf97551d5
Saalim Quadri <danascape@gmail.com>
2025-02-10 13:57:45 +0530

    setup: Install libnl
    * For nfs-utils

--- setup/build-env.sh
+++ setup/build-env.sh
@@ -11,6 +11,7 @@ sudo DEBIAN_FRONTEND=noninteractive \
 	git zip ftp gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi \
 	libssl-dev lftp zstd wget libelf-dev libfl-dev clang flex bison cpio \
 	libyaml-dev golang-go python3-pip swig shellcheck jq shfmt kpartx \
+ 	libnl-3-dev libnl-genl-3-dev \
 	qemu-system-x86
 
 pip3 install dtschema yamllint python-magic flake8
