4654a67d35ed0cbcbf870ff56b020ea4fa498623
Saalim Quadri <danascape@gmail.com>
2025-03-23 04:00:40 +0530

    env: Add Toolchain paths and LC stuff
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- env/setup_kernel.sh
+++ env/setup_kernel.sh
@@ -4,3 +4,17 @@
 # Copyright (C) 2019 Saalim Quadri <danascape@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
+
+# Setup Toolchain paths
+# Set current path
+WORK_DIR="/mnt/kernel"
+TOOLCHAINS_PATH="$WORK_DIR/toolchains"
+
+export PATH="${TOOLCHAINS_PATH}/toolchains/clang-11/bin:${TOOLCHAINS_PATH}/toolchains/gcc64/bin:${TOOLCHAINS_PATH}/toolchains/gcc32/bin:${TOOLCHAINS_PATH}/toolchains/prebuilts-misc/linux-x86/libufdt:${PATH}"
+
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+ulimit -n 4096
