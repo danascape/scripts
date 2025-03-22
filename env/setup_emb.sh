5182b99bd41e5e361505ab1acd23206184a3bdc9
Saalim Quadri <danascape@gmail.com>
2025-03-23 04:17:03 +0530

    env: Set cache paths if exists
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- env/setup_emb.sh
+++ env/setup_emb.sh
@@ -12,5 +12,16 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 ulimit -n 4096
 
-eval export DL_DIR="$(pwd)"/../downloads/
-eval export SSTATE_DIR="$(pwd)"/../sstate-cache/
+#  Env Variables
+CURRENT_DIR="$PWD"
+
+# Check if caching dirs exist
+if [ -d "$CURRENT_DIR"/../downloads ] || [ -d "$CURRENT_DIR"/../sstate-cache ]; then
+	echo "env: Setting cached dirs path"
+	eval export DL_DIR="$(pwd)"/../downloads/
+    eval export SSTATE_DIR="$(pwd)"/../sstate-cache/
+else
+	echo "env: Cached paths does not exist. Skipping!"
+    echo "env: Good luck! with clean build"
+	return
+fi
